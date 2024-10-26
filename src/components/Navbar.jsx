dade91cc3ae2c43801ecbd7059988a879cdf3bc1
RandyFonsec <rfonriv@gmail.com>
2024-10-26 16:04:56 -0600

    Navbar and logo fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import { React, useState } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+  const mobileMenuRef = useRef(null); // Referencia para el menú móvil
+
+  // Cierra el menú si se hace clic fuera del menú móvil
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (mobileMenuRef.current && !mobileMenuRef.current.contains(event.target)) {
+        setShow(false); // Cierra el menú si el clic es fuera del menú
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Limpia el event listener al desmontar el componente
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [mobileMenuRef]);
 
   return (
     <>
@@ -23,7 +40,8 @@ function Navbar() {
           {!show && <button className={styles.hamburgerButton} onClick={() => setShow(true)}>☰</button>}
         </div>
 
-        <div className={`${styles.mobileMenu} ${show ? styles.show : ''}`}>
+        {/* Menu Móvil */}
+        <div ref={mobileMenuRef} className={`${styles.mobileMenu} ${show ? styles.show : ''}`}>
           <button className={styles.hamburgerButton} onClick={() => setShow(false)}>X</button>
           <Link to="/products" className={styles.linkButton}>Products</Link>
           <Link to="/about" className={styles.linkButton}>About Us</Link>
