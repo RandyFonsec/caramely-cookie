34dec2371ca9ab01950182b1d88aa501f16cd71f
Erick13as <alvaradosolanoerick@gmail.com>
2024-10-28 12:57:59 -0600

    Update AboutGrid.jsx

--- src/components/AboutGrid.jsx
+++ src/components/AboutGrid.jsx
@@ -1,30 +1,31 @@
 import styles from './AboutGrid.module.css';
 import AboutCard from './AboutCard';
+import logo from "../assets/caramely-cookie_secundario_red.png";
 
 const AboutGrid = () => {
   const aboutItems = [
     {
       title: "Mission",
       description: "Bringing smiles to people's faces with delicious, high-quality products.",
-      image: "/src/assets/caramely-cookie_secundario_red.png",
+      image: logo,
       backgroundColor: "#FFA07A" 
     },
     {
       title: "Vision",
       description: "To be the preferred choice for fresh baked cookies and cookie desserts nationwide.",
-      image: "/src/assets/caramely-cookie_secundario_red.png",
+      image: logo,
       backgroundColor: "#FFB6C1" 
     },
     {
       title: "Values",
       description: "We use premium, local and fresh ingredients, along with sustainable packaging, to create the highest quality cookies.",
-      image: "/src/assets/caramely-cookie_secundario_red.png",
+      image: logo,
       backgroundColor: "#FFB6C1"
     },
     {
       title: "Value Proposition",
       description: "Premium Ingredients, Addictive Cookies.",
-      image: "/src/assets/caramely-cookie_secundario_red.png",
+      image: logo,
       backgroundColor: "#FFA07A"
     }
   ];
